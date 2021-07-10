ee7be6971cf5691fbbde17b94e351935b3e1b283
Hussein Hamed <husseinhamed811@gmail.com>
2021-07-10 20:02:29 +0200

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
     // SystemChrome.setPreferredOrientations([
     //   DeviceOrientation.portraitUp,
     //   DeviceOrientation.portraitDown,
-    // ]);
+    // ]);//////////////////////////////////////////////////////////////////////////////////
     return  MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => AppCubit(repository)),
